f18bf204375d8a00d43627e90ae203ed17099ce2
Divya <Divya@10.0.0.174>
2017-04-21 17:18:53 -0400

    Revert "Working on Dev Tools"
    This reverts commit 36b1bf9ee15b175a5d063ec9ce1fada8784e9e8d.

--- src/main/java/com/project/webapp/devtools/converters/Time.java
+++ src/main/java/com/project/webapp/devtools/converters/Time.java
@@ -3,13 +3,10 @@
  */
 package com.project.webapp.devtools.converters;
 
-import org.springframework.web.bind.annotation.RestController;
-
 /**
  * @author Vamsi Ravi
  *
  */
-@RestController
 public class Time {
 
 }
