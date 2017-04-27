b6e4d8b669710822c11b87985505e53024001f37
Vamsi Ravi <ivamsiravi@gmail.com>
2017-04-27 09:54:42 -0400

    Working on Temperature Conversions

--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@ Utility API for the Developers: One Stop for all the Developer Daily Tasks
 
 Some of the Rest WebServices Developed so far: -- UUID Generator -- Random Generators -- Math Tools -- String Manipulation -- BarCodeGenerators -- Network Test Tools -- Encryptors and Decryptors -- JSON Parser
 
-Planning to work the following for the project: -- CodeBeautifier -- CodeFormatter -- CodeMinifier -- Decompiler -- File Conversion Tools -- Password Generators
+Planning to work the following for the project: -- CodeBeautifier -- CodeFormatter -- CodeMinifier -- Decompiler -- File Conversion Tools -- Password Generators -- Timer -- Stop Watch
 
 If you have questions or need help with any of the above services. Feel free to reach me
 
--- src/main/java/com/project/webapp/devtools/converters/Temperature.java
+++ src/main/java/com/project/webapp/devtools/converters/Temperature.java
@@ -3,10 +3,50 @@
  */
 package com.project.webapp.devtools.converters;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author Vamsi Ravi
  *
  */
+@RestController
 public class Temperature {
 
+	@RequestMapping(value="fahrenheit/{id}/celsius", method=RequestMethod.GET)
+	public Double fahrenheitToCelsius(@PathVariable Double id){
+		return (id - 32) * (5d / 9d);
+	}
+	
+	@RequestMapping(value="celsius/{id}/fahrenheit", method=RequestMethod.GET)
+	public Double celsiusToFahrenheit(@PathVariable Double id){
+		return 32 + (id * 9d / 5d);
+	}
+	
+	@RequestMapping(value="kelvin/{id}/celsius", method=RequestMethod.GET)
+	public Double kelvinToCelsius(@PathVariable Double id){
+		return id - 273.15f;
+	}
+	
+	@RequestMapping(value="celsius/{id}/kelvin", method=RequestMethod.GET)
+	public Double celsiusToKelvin(@PathVariable Double id){
+		return id + 273.15f;
+	}
+	
+	@RequestMapping(value="kelvin/{id}/fahrenheit", method=RequestMethod.GET)
+	public Double kelvinToFahrenheit(@PathVariable Double id){
+		return (( id - 273.15) * 9d/5d) + 32;
+	}
+	
+	@RequestMapping(value="fahrenheit/{id}/kelvin", method=RequestMethod.GET)
+	public Double fahrenheitToKelvin(@PathVariable Double id){
+		return (5d/9d * (id - 32) + 273.15);
+	}
+	
+	
+	// (5/9 * (fahrenheit - 32) + 273.15);
+	
+	
 }
