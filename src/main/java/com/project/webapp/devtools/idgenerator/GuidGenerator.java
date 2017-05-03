80431e746ce15971c33617175c3035412079e8a9
Vamsi Ravi <ivamsiravi@gmail.com>
2017-05-02 22:49:44 -0400

    Changes for the GUID Generator

--- src/main/java/com/project/webapp/devtools/idgenerator/GuidGenerator.java
+++ src/main/java/com/project/webapp/devtools/idgenerator/GuidGenerator.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.project.webapp.devtools.model.UUIDModel;
 import com.project.webapp.devtools.util.ConstantValues;
 
 /**
@@ -23,42 +24,72 @@ import com.project.webapp.devtools.util.ConstantValues;
 @RestController
 public class GuidGenerator {
 
-	@RequestMapping(value="guid/generator/setgetgo", method= RequestMethod.GET)
+	/*This Set Get Go Doesn't Work
+	 * 
+	 * @RequestMapping(value="guid/generator/setgetgo", method= RequestMethod.GET)
 	public String guidGeneratorSetGetGo(){
 		
 		Map<String, String> variables = new HashMap<String, String>();
-		
+		//UUIDModel uuidModel = new UUIDModel();
 		RestTemplate restTemplate = new RestTemplate();
-		
-		String response = restTemplate.getForObject(ConstantValues.GUID_GENERATOR_URL, String.class, variables );
+		return restTemplate.getForObject(ConstantValues.GUID_GENERATOR_URL, String.class, variables );
+		//String response = restTemplate.getForObject(ConstantValues.GUID_GENERATOR_URL, String.class, variables );
 				
-		return response;
-	}
+//		return uuidModel;
+	}*/
 	
 	@RequestMapping(value="guid/generator", method= RequestMethod.GET)
-	public String guidGenerator(){
+	public UUIDModel guidGenerator(){
 		
 		Map<String, String> variables = new HashMap<String, String>();
-		
+		UUIDModel uuidModel = new UUIDModel();
 		RestTemplate restTemplate = new RestTemplate();
 		
-		String response = restTemplate.getForObject(ConstantValues.GUID_MULTIPLE_GENERATOR_URL, String.class, variables );
+		uuidModel.setUuid(restTemplate.getForObject(ConstantValues.GUID_MULTIPLE_GENERATOR_URL, String.class, variables ).trim());
 				
-		return response.trim();
+		return uuidModel;
+	}
+	
+	@RequestMapping(value="guid/generator/dash", method= RequestMethod.GET)
+	public UUIDModel guidGeneratorWithoutDash(){
+		
+		Map<String, String> variables = new HashMap<String, String>();
+		UUIDModel uuidModel = new UUIDModel();
+		RestTemplate restTemplate = new RestTemplate();
+		
+		uuidModel.setUuid(restTemplate.getForObject(ConstantValues.GUID_MULTIPLE_GENERATOR_URL, String.class, variables ).trim().replaceAll("-", ""));
+				
+		return uuidModel;
 	}
 	
 	@RequestMapping(value="guid/generator/{id}", method= RequestMethod.GET)
-	public List<String> guidMultipleGenerator(@PathVariable("id") Integer id){
+	public List<UUIDModel> guidMultipleGenerator(@PathVariable("id") Integer id){
 		
-		List<String> guidList = new ArrayList<String>();
+		List<UUIDModel> guidList = new ArrayList<UUIDModel>();
 		for(int i = 0 ; i < id; i++){
 			Map<String, String> variables = new HashMap<String, String>();
-			
+			UUIDModel uuidModel = new UUIDModel();
 			RestTemplate restTemplate = new RestTemplate();
 			
-			String response = restTemplate.getForObject(ConstantValues.GUID_MULTIPLE_GENERATOR_URL, String.class, variables );
-			guidList.add(response.trim());
+			uuidModel.setUuid( restTemplate.getForObject(ConstantValues.GUID_MULTIPLE_GENERATOR_URL, String.class, variables ).trim());
+			guidList.add(uuidModel);
 		}		
 		return guidList;
 	}
+	
+	@RequestMapping(value="guid/generator/{id}/dash", method= RequestMethod.GET)
+	public List<UUIDModel> guidMultipleGeneratorWithoutDash(@PathVariable("id") Integer id){
+		
+		List<UUIDModel> guidList = new ArrayList<UUIDModel>();
+		for(int i = 0 ; i < id; i++){
+			Map<String, String> variables = new HashMap<String, String>();
+			UUIDModel uuidModel = new UUIDModel();
+			RestTemplate restTemplate = new RestTemplate();
+			uuidModel.setUuid( restTemplate.getForObject(ConstantValues.GUID_MULTIPLE_GENERATOR_URL, String.class, variables ).trim().replaceAll("-", ""));
+			guidList.add(uuidModel);
+		}		
+		return guidList;
+	}
+	
+	
 }
