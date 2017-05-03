22365066edfb2ca6e7d4b1966813702933b4ed48
Vamsi Ravi <ivamsiravi@gmail.com>
2017-05-03 10:57:35 -0400

    Dictionary API Complete

---
+++ src/main/java/com/project/webapp/devtools/model/DictionaryModel.java
@@ -0,0 +1,86 @@
+/**
+ * 
+ */
+package com.project.webapp.devtools.model;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * @author Vamsi Ravi
+ *
+ */
+
+public class DictionaryModel {
+
+
+	@JsonProperty
+	public String id = null;
+	
+	@JsonProperty
+	public String term = null;
+	
+	@JsonProperty
+	public String definition = null;
+	
+	@JsonProperty
+	public String example = null;
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the term
+	 */
+	public String getTerm() {
+		return term;
+	}
+
+	/**
+	 * @param term the term to set
+	 */
+	public void setTerm(String term) {
+		this.term = term;
+	}
+
+	/**
+	 * @return the definition
+	 */
+	public String getDefinition() {
+		return definition;
+	}
+
+	/**
+	 * @param definition the definition to set
+	 */
+	public void setDefinition(String definition) {
+		this.definition = definition;
+	}
+
+	/**
+	 * @return the example
+	 */
+	public String getExample() {
+		return example;
+	}
+
+	/**
+	 * @param example the example to set
+	 */
+	public void setExample(String example) {
+		this.example = example;
+	}
+	
+}
