805adaf1074cbbafbb282becdb7dad9570cab01b
Vamsi Ravi <ivamsiravi@gmail.com>
2017-05-06 23:03:03 -0400

    Network Utils to resolve Ip Address and Ping remote server

--- src/main/java/com/project/webapp/devtools/ipaddress/IpAddress.java
+++ src/main/java/com/project/webapp/devtools/ipaddress/IpAddress.java
@@ -3,21 +3,16 @@
  */
 package com.project.webapp.devtools.ipaddress;
 
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import org.apache.tomcat.jni.Address;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import com.project.webapp.devtools.util.ConstantValues;
 
 /**
  * @author Vamsi Ravi
@@ -39,13 +34,34 @@ public class IpAddress {
 		return inetAddressList;
 	}
 	
-	// Doesn't Work
-	@RequestMapping(value="ip/address/{id}", method=RequestMethod.GET)
-	public String getIpAddressDetails(@PathVariable String id){
-		RestTemplate restTemplate = new RestTemplate();
-		Map<String, String> variables = new HashMap<String, String>();
-		String result = restTemplate.getForObject("https://freegeoip.net/json/"+id,  String.class, variables);
-		return result;
+
+	@RequestMapping(value="ipaddress/{id}/resolve", method=RequestMethod.GET)
+	public String getIpAddressByHostName(@PathVariable("id") String id) throws UnknownHostException{
+		
+		InetAddress inetAddress = InetAddress.getByName(id);
+		
+		return inetAddress.getHostAddress();
 	}
+	
+	@RequestMapping(value="hostname/{id}/resolve", method=RequestMethod.GET)
+	public String getHostNameByIpAddress(@PathVariable("id") String id) throws UnknownHostException{
+		
+		InetAddress inetAddress = InetAddress.getByName(id);
+		
+		return inetAddress.getHostName();
+	}
+	
+	@RequestMapping(value="ping/{id}/resolve", method=RequestMethod.GET)
+	public String pingIpAddressOrHost(@PathVariable("id") String id) throws IOException{
+		
+		InetAddress inetAddress = InetAddress.getByName(id);
+		if(inetAddress.isReachable(5000)){
+			return "Host is Reachable";
+		}else{
+			return "Host is not Reachable";
+		}
+	}
+	
+	
 	
 }
