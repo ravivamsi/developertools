ab4d8d2d35aec988c987a27508c4cc0b808768ac
Vamsi Ravi <ivamsiravi@gmail.com>
2017-05-22 20:36:53 -0400

    Convert JAR to WAR

--- src/main/java/com/project/webapp/devtools/DevtoolsApplication.java
+++ src/main/java/com/project/webapp/devtools/DevtoolsApplication.java
@@ -2,10 +2,18 @@ package com.project.webapp.devtools;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DevtoolsApplication {
+public class DevtoolsApplication extends SpringBootServletInitializer{
 
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(DevtoolsApplication.class);
+	    }
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DevtoolsApplication.class, args);
 	}
